f62a718895bb1df3efe4cc6b37e2f16120699a69
Ashok Menon <ashokmenon@fb.com>
2022-09-26 01:02:00 +0100

    [Early Init] Chunk up LIBRARY_PATH

--- early-init.el
+++ early-init.el
@@ -4,5 +4,9 @@
 (push "/opt/homebrew/bin" exec-path)
 (push "~/.local/bin" exec-path)
 
-(setenv "LIBRARY_PATH"
-        "/opt/homebrew/opt/gcc/lib/gcc/current:/opt/homebrew/opt/libgccjit/lib/gcc/current:/opt/homebrew/opt/gcc/lib/gcc/current/gcc/aarch64-apple-darwin21/12")
+(setenv
+ "LIBRARY_PATH"
+ (concat
+  "/opt/homebrew/opt/gcc/lib/gcc/current"
+  ":/opt/homebrew/opt/libgccjit/lib/gcc/current"
+  ":/opt/homebrew/opt/gcc/lib/gcc/current/gcc/aarch64-apple-darwin21/12"))
