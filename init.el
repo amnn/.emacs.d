b9e9d53acfb1f613251902c6a172755cd49d0ea8
Ashok Menon <ashokmenon@fb.com>
2022-09-28 00:24:34 +0100

    [Fish] Major mode for fish shell scripts

--- init.el
+++ init.el
@@ -375,6 +375,8 @@
 
 ;;; Language Major Modes =================================================== ;;;
 
+(use-package fish-mode :ensure t)
+
 (use-package markdown-mode
   :ensure t
   :mode ("README\\.md\\'" . gfm-mode))
